d7d02a24e8106d78ab0cd26a7c5a1f5117a7810b
chris <bbhriss@gmail.com>
2025-11-13 18:15:38 -0800

    created shooting stars

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { LoadingScreen } from './components/LoadingScreen';
+import "./index.css";
 
 function App() {
   const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(false);
+
 
   return (
     <>
-    <LoadingScreen />
+    {!loading && <LoadingScreen onComplete={() => setLoading(true)} />}
+      <body>
+        <section-test>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </section-test>
+      </body>
     </>
+    
   )
 }
 
